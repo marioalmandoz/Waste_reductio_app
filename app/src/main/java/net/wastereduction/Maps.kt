fab1cc66be282e973a154e9e8e5f096d35dc16c4
Mario <m.almandozlatierro@students.fontys.nl>
2023-09-27 10:36:48 +0200

    Maps

--- app/src/main/java/net/wastereduction/Maps.kt
+++ app/src/main/java/net/wastereduction/Maps.kt
@@ -4,9 +4,12 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
+import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
 
 class Maps : AppCompatActivity(), OnMapReadyCallback {
 
@@ -18,12 +21,22 @@ class Maps : AppCompatActivity(), OnMapReadyCallback {
         createFragment()
     }
 
-    private fun createFragment(){
+    private fun createFragment(){// para crear el fragmento
         val mapFragment : SupportMapFragment = supportFragmentManager.findFragmentById(R.id.map) as SupportMapFragment
         mapFragment.getMapAsync(this)
     }
 
-    override fun onMapReady(googleMap: GoogleMap) {
+    override fun onMapReady(googleMap: GoogleMap) {// es un metodo creado por añadir OnMapReadyCallback
         map = googleMap
+        createMarker()
+    }
+
+    private fun createMarker(){ // para añadir puntos en el mapa y animaciones de enfoque automaticos
+        val coordinates = LatLng(51.451587, 5.453913)
+        val marker : MarkerOptions = MarkerOptions().position(coordinates).title("ICT University")
+        map.addMarker(marker)
+        map.animateCamera(
+                CameraUpdateFactory.newLatLngZoom(coordinates, 18f), 4000,null   // 4000 es un segundo
+        )
     }
 }
